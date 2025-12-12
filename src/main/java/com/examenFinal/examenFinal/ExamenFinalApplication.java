eb9046fa442dfa1966550d105a5617fa354291a5
Eris <erikaalejandrass@ufps.edu.co>
2025-12-11 19:43:27 -0500

    Prueba 2

--- src/main/java/com/examenFinal/examenFinal/ExamenFinalApplication.java
+++ src/main/java/com/examenFinal/examenFinal/ExamenFinalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExamenFinalApplication {
-//prueba de credenciales
+//zorro me cae mal
 	public static void main(String[] args) {
 		SpringApplication.run(ExamenFinalApplication.class, args);
 	}
